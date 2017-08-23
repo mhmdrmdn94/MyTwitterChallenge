70d833f05d4302b206efd2826ead7f48366736e9
MRamadan@ntg <ntg@Mohammads-MacBook-Pro.local>
2017-08-23 17:08:42 +0200

    Initial commit

---
+++ TwitterChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterChallenge
+//
+//  Created by MRamadan@ntg on 8/23/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
