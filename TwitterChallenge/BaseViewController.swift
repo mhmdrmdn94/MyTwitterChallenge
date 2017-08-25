fb4e61ad11d6c50fe6dc032ca59b950391fa3795
MRamadan@ntg <ntg@Mohammads-MacBook-Pro.local>
2017-08-25 13:37:44 +0200

    Implementing BaseViewController: showAlert() and showProgressBar()

--- TwitterChallenge/BaseViewController.swift
+++ TwitterChallenge/BaseViewController.swift
@@ -6,4 +6,45 @@
 //
 //
 
-import Foundation
+import UIKit
+import MBProgressHUD
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    //MARK:-  ProgresBar initialization func
+    
+    func showMBProgressBar(view:UIView!,title: String?) -> MBProgressHUD{
+        
+        let hud = MBProgressHUD.showAdded(to: view, animated: true)
+        
+        if title == nil{
+            hud.label.text = NSLocalizedString("Loading ...", comment: "Loading ...")
+        }else{
+            hud.label.text = title!
+        }
+        
+        return hud
+    }
+    
+    
+    
+    //MARK:-  Alert initialization func.
+
+    func showAlert(message: String, title: String = "") {
+       
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+}
